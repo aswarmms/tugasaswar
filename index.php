45e402c6823fb3b1b203421ae98605fffd4bf12b
aswarmms <46174944+aswarmms@users.noreply.github.com>
2018-12-27 10:12:50 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+echo "
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Cloud </title>
+</head>
+<body>
+	<marquee width="500" height="40">Nama Saya Aswar Nim 215280055</marquee>
+</body>
+</html>
+";
+?>
